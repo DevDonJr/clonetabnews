9a3d058228701d572eb97672ced21e59f50f4df1
Donato Junior <103531835+DevDonJr@users.noreply.github.com>
2024-07-21 21:15:35 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clonetabnews
+
 Projeto do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home( ){
-    return <><h1>Deh, te amoo! :)</h1>
-        <h2>SE ME AMA DA UMA RISADINHA</h2></>;
+function Home() {
+  return (
+    <>
+      <h1>Deh, te amoo! :)</h1>
+      <h2>SE ME AMA DA UMA RISADINHA</h2>
+    </>
+  );
 }
 export default Home;
